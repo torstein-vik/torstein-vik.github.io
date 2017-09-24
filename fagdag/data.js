596895e35f9183457e581651ccdbca6d639dc551
Torstein Vik <torsteinv64@gmail.com>
2017-09-24 23:12:41 +0200

    Initial Commit + Fagdag

---
+++ fagdag/data.js
@@ -0,0 +1,62 @@
+/*
+
+0 is ordinary friday,
+-1 is vacation,
+1-6 are the specific subjects,
+text is something else
+
+*/
+
+var weekSpec = [
+    0,
+    'Skolevalgdebatt',
+    6,
+    2,
+    1,
+    4,
+    5,
+    -1,
+    3,
+    0,
+    6,
+    2,
+    1,
+    4,
+    5,
+    3,
+    6,
+    -1,
+    -1,
+    2,
+    1,
+    4,
+    5,
+    3
+]
+
+
+// List of the first four subjects
+var personSpec = {
+    'Karoline': [['Matematikk S2', 'A527'], 0, 0, ['Kjemi 2', 'A429, A431']],
+    'Elias':    [0, ['Matematikk R2', 'B339'], 0, ['Fysikk 2', 'A439, A441']],
+    'Ruben':    [['Biologi 2', 'A427'], ['Matematikk S2', 'A509'], ['IT 2', 'A533'], 0],
+    'Isak':     [0,0,['Samføk. 2', 'A526'], ['Pol. og menneskeret.', 'A509']],
+    'Frida':    [['Matematikk S2', 'A527'], 0, ['Tysk 1 + 2', 'A360'], ['Kjemi 2', 'A429, A431']],
+    'Elida':    [['Rettslære 1', 'A533'], ['Rettslære 2', 'B410'], ['Eng. spr. litt. og kult.', 'A512']],
+    'William':  [['Kjemi 2', 'A429, A431'], ['Matematikk R2', 'B339'], 0, 0],
+    'Levi':     [0,['Matematikk R2', 'B339'], ['Psykologi 1', 'A527'], ['Fysikk 2', 'A439, A441']],
+    'Kristian': [0,0,['Samføk. 2', 'A526'], ['Pol. og menneskeret.', 'A509']],
+    'Henrik':   [['Kjemi 2', 'A429, A431'], ['Matematikk R2', 'B339'], 0, ['Fysikk 2', 'A439, A441']],
+    'Håvard':   [['Kjemi 2', 'A429, A431'], ['Matematikk R2', 'B339'], 0, 0],
+    'Sander':   [['Økonomistyring', 'A537'], ['Matematikk S2', 'A509'], ['Samføk. 2', 'A526'], 0],
+    'Emil':     [['Internasj. engelsk', 'A542'],0,0,['Fysikk 2', 'A439, A441']],
+    'Ludvik':   [0,0,0,['Fysikk 2', 'A439, A441']],
+    'Linda':    [0,['Matematikk R2', 'B339'], ['IT 2', 'A533'], 0],
+    'Preben':   [0,['Kjemi 1', 'A429, A431'], ['IT 2', 'A533'], ['Fysikk 2', 'A439, A441']],
+    'Torstein': [0,0,0,0],
+    'Malene':   [0,['Matematikk R2', 'B339'],['Samføk. 2', 'A526'],0],
+    'Steinar':  [['Biologi 2', 'A427'], 0, ['Tysk 1 + 2', 'A360'], ['Kjemi 2', 'A429, A431']]
+
+}
+
+var commonSpec = [['Norsk', '???'], ['RLE', 'B410']]
---
+++ fagdag/index.js
@@ -0,0 +1,116 @@
+var weeknum;
+var personname = "";
+
+$(function(){
+
+
+    setWeekNum(getWeekNumber(new Date()) - 34);
+
+    $.each(personSpec, function(name){
+        $("#setname > select").append("<option>"+name+"</option>");
+    });
+
+    $("#setname > select").change(function(){
+        setName($("#setname > select option:selected").html());
+        $("#setname > select option:disabled").prop('selected', true);
+    });
+
+    var name = localStorage.getItem('fagdagName');
+
+    if(name){
+        setName(name);
+    }
+});
+
+function setName(name){
+    $("#setname").hide();
+
+    localStorage.setItem('fagdagName', name);
+    personname = name;
+
+    resolveOutput();
+
+    $("#output").show();
+}
+
+function unsetName(){
+    $("#output").hide();
+
+    localStorage.removeItem('fagdagName');
+
+    $("#setname").show();
+}
+
+function setWeekNum(week) {
+    week = week % 52;
+    if( week < 0) week += 52;
+
+    weeknum = week;
+}
+
+function resolveOutput(){
+    $("#name").html(personname);
+
+    var date = getDateOfISOWeek((weeknum + 34) % 52, 2017);
+
+    date.setDate(date.getDate() + 4);
+
+    $("#date").html(date.toLocaleDateString("nb-NO", {weekday: 'long', month: 'long', day: 'numeric'}));
+
+    if (weeknum >= 0 && weeknum < weekSpec.length) {
+        var week = weekSpec[weeknum];
+
+        var subject = "";
+        var room = "";
+
+        if (week === 0){
+            subject = "Ordinær timeplan";
+        } else if (week === -1) {
+            subject = "FRI!";
+        } else if (typeof week === 'number' && week >= 1 && week <= 4){
+            var data = personSpec[personname][week - 1];
+            if (data == 0) {
+                subject = "FRI!";
+            } else {
+                subject = data[0];
+                room    = data[1];
+            }
+        } else if (typeof week === 'number' && week >= 5 && week <= 6){
+            var data = commonSpec[week - 5];
+            if (data == 0) {
+                subject = "FRI!";
+            } else {
+                subject = data[0];
+                room    = data[1];
+            }
+        } else if (typeof week === 'string') {
+            subject = week;
+        } else {
+            alert("Systemfeil. Si det til Torstein.");
+        }
+
+        $("#subject").html(subject);
+        $("#room")   .html(room);
+    } else {
+        $("#subject").html("Denne uken er ikke lagt til");
+    }
+}
+
+function getWeekNumber(d) {
+    d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+    d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay()||7));
+    var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
+    var weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7);
+    return weekNo;
+}
+
+function getDateOfISOWeek(w, y) {
+    var simple = new Date(y, 0, 1 + (w - 1) * 7);
+    var dow = simple.getDay();
+    var ISOweekStart = simple;
+    if (dow <= 4)
+        ISOweekStart.setDate(simple.getDate() - simple.getDay() + 1);
+    else
+        ISOweekStart.setDate(simple.getDate() + 8 - simple.getDay());
+    return ISOweekStart;
+}
